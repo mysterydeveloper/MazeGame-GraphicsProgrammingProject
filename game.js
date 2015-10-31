2de228ee661c848dccb36bbb92d9cc396dc283e4
mysterydeveloper <manus.duggan@yahoo.com>
2015-10-31 20:48:18 +0000

    shell of game
    the shell of the game with all of the necessary functions

---
+++ game.js
@@ -0,0 +1,37 @@
+
+var canvas = document.getElementById("canvas");
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight
+var context = canvas.getContext("2d");
+
+function unloadScrollBars() {
+    document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+    document.body.scroll = "no"; // ie only
+}
+unloadScrollBars();
+var playerobj = {
+	
+}
+ 
+function maze() {
+  
+}
+maze();
+
+function mazedraw(){
+	
+}
+
+function main(){
+
+	window.requestAnimationFrame(main);
+}
+
+
+window.requestAnimationFrame(main);
+
+window.addEventListener("keydown", function(event) { 
+
+
+
+});
